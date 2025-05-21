30a45c557f12a8287dc61ed901f70c3bd05864de
Шкуропатова Екатерина <katygetty98@mail.ru>
2025-05-22 01:17:16 +0700

    изменения1

--- main.py
+++ main.py
@@ -686,7 +686,7 @@ def allowed_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
 
 
 
